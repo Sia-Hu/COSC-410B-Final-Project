14edc4b52a73aa32338a864f7f34dbb5d9e6f7eb
胡夕颜 <huxiyan@Sias-MacBook.local>
2025-04-16 16:51:11 -0400

    hinge

---
+++ ResNetHinge.py
@@ -0,0 +1,259 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+import os
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+from PIL import Image
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Input, Activation, Add, Dense, Conv2D, GlobalAveragePooling2D, MaxPooling2D
+from keras.layers import BatchNormalization, Dropout
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras.optimizers import Adam
+from keras.callbacks import ReduceLROnPlateau, EarlyStopping
+from keras.models import Model
+from keras.utils import plot_model
+from sklearn.metrics import classification_report, confusion_matrix
+
+
+dataset_path = '/home/xhu/COSC410/TrashType_Image_Dataset'
+
+garbage_types = os.listdir(dataset_path)
+
+all_dimensions_set = set()
+
+dat = []
+
+for garbage_type in garbage_types:
+    folder_path = os.path.join(dataset_path, garbage_type)
+    for file in os.listdir(folder_path):
+        dat.append((os.path.join(dataset_path, garbage_type, file), garbage_type))
+
+df = pd.DataFrame(dat, columns=['filepath', 'label'])
+
+train_df, val_df = train_test_split(df, test_size=0.2, random_state=42, stratify=df['label'])
+
+
+# Slight Augmentation settings for training
+train_datagen = ImageDataGenerator(
+    rescale=1./255,                     # Normalize pixel values to [0,1]
+    rotation_range=45,                  # Randomly rotate the images by up to 45 degrees
+    width_shift_range=0.15,             # Randomly shift images horizontally by up to 15% of the width
+    height_shift_range=0.15,            # Randomly shift images vertically by up to 15% of the height
+    zoom_range=0.15,                    # Randomly zoom in or out by up to 15%
+    horizontal_flip=True,               # Randomly flip images horizontally
+    vertical_flip=True,                 # Randomly flip images vertically
+    shear_range=0.05,                   # Apply slight shear transformations
+    brightness_range=[0.9, 1.1],        # Vary brightness between 90% to 110% of original
+    channel_shift_range=10,             # Randomly shift channels (can change colors of images slightly but less aggressively)
+    fill_mode='nearest'                 # Fill in missing pixels using the nearest filled value
+)
+
+# Only rescaling for validation
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+
+# Using flow_from_dataframe to generate batches
+# Generate training batches from the training dataframe
+train_generator = train_datagen.flow_from_dataframe(
+    dataframe=train_df,                  # DataFrame containing training data
+    x_col="filepath",                    # Column with paths to image files
+    y_col="label",                       # Column with image labels
+    target_size=(384, 384),              # Resize all images to size of 384x384
+    batch_size=32,                       # Number of images per batch
+    class_mode='categorical',            # One-hot encode labels
+    seed=42,                             # Seed for random number generator to ensure reproducibility
+    shuffle=False                        # Data is not shuffled; order retained from DataFrame
+)
+
+
+# Generate validation batches from the validation dataframe
+val_generator = val_datagen.flow_from_dataframe(
+    dataframe=val_df,                    # DataFrame containing validation data
+    x_col="filepath",                    # Column with paths to image files
+    y_col="label",                       # Column with image labels
+    target_size=(384, 384),              # Resize all images to size of 384x384
+    batch_size=32,                       # Number of images per batch
+    class_mode='categorical',            # One-hot encode labels
+    seed=42,                             # Seed for random number generator to ensure reproducibility
+    shuffle=False                        # Data is not shuffled; order retained from DataFrame
+)
+
+
+class_labels = train_df['label'].unique()
+
+
+def residual_block(X, kernel_size, filters, reduce=False, stride=2):
+    """
+    Implement a residual block for ResNet architectures.
+    
+    Arguments:
+    X           -- input tensor of shape (m, height, width, channels)
+    kernel_size -- integer, kernel size of the middle convolutional layer in the main path
+    filters     -- python list of integers, defining the number of filters in the CONV layers of the main path
+    reduce      -- boolean, whether to reduce the spatial dimensions and increase depth; 
+                    if True, applies 1x1 CONV layer to the shortcut path.
+    stride      -- integer, strides for the convolutional layer
+    
+    Returns:
+    X           -- output of the identity block, tensor of shape (height, width, channels)
+    """
+    
+    # Retrieve Filters
+    F1, F2, F3 = filters
+    
+    # Save the input value. We will need this later to add back to the main path. 
+    X_shortcut = X
+    
+    if reduce:
+        # if we are to reduce the spatial size, apply a 1x1 CONV layer to the shortcut path
+        X = Conv2D(filters = F1, kernel_size = (1, 1), strides = (stride,stride), padding = 'valid', kernel_initializer='he_normal')(X)
+        X = BatchNormalization(axis = 3)(X)
+        X = Activation('relu')(X)
+        
+        X_shortcut = Conv2D(filters = F3, kernel_size = (1, 1), strides = (stride,stride), padding = 'valid', kernel_initializer='he_normal')(X_shortcut)
+        X_shortcut = BatchNormalization(axis = 3)(X_shortcut)
+    else: 
+        # First component of main path
+        X = Conv2D(filters = F1, kernel_size = (1, 1), strides = (1,1), padding = 'valid', kernel_initializer='he_normal')(X)
+        X = BatchNormalization(axis = 3)(X)
+        X = Activation('relu')(X)
+    
+    # Second component of main path
+    X = Conv2D(filters = F2, kernel_size = (kernel_size, kernel_size), strides = (1,1), padding = 'same', kernel_initializer='he_normal')(X)
+    X = BatchNormalization(axis = 3)(X)
+    X = Activation('relu')(X)
+
+    # Third component of main path
+    X = Conv2D(filters = F3, kernel_size = (1, 1), strides = (1,1), padding = 'valid', kernel_initializer='he_normal')(X)
+    X = BatchNormalization(axis = 3)(X)
+
+    # Final step: Add shortcut value to main path, and pass it through a ReLU activation 
+    X = Add()([X, X_shortcut])
+    X = Activation('relu')(X)
+    
+    return X
+
+def Hinge_ResNet50(input_shape, classes):
+
+    # Define the input as a tensor with shape input_shape
+    X_input = Input(input_shape)
+
+    # Stage 1
+    X = Conv2D(64, (7, 7), strides=(2, 2), kernel_initializer='he_normal')(X_input)
+    X = BatchNormalization(axis=3)(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((3, 3), strides=(2, 2))(X)
+
+    # Stage 2
+    X = residual_block(X, 3, [64, 64, 256], reduce=True, stride=1)
+    X = residual_block(X, 3, [64, 64, 256])
+    X = residual_block(X, 3, [64, 64, 256])
+
+    # Stage 3 
+    X = residual_block(X, 3, [128, 128, 512], reduce=True, stride=2)
+    X = residual_block(X, 3, [128, 128, 512])
+    X = residual_block(X, 3, [128, 128, 512])
+    X = residual_block(X, 3, [128, 128, 512])
+
+    # Stage 4 
+    X = residual_block(X, 3, [256, 256, 1024], reduce=True, stride=2)
+    X = residual_block(X, 3, [256, 256, 1024])
+    X = residual_block(X, 3, [256, 256, 1024])
+    X = residual_block(X, 3, [256, 256, 1024])
+    X = residual_block(X, 3, [256, 256, 1024])
+    X = residual_block(X, 3, [256, 256, 1024])
+
+    # Stage 5 
+    X = residual_block(X, 3, [512, 512, 2048], reduce=True, stride=2)
+    X = residual_block(X, 3, [512, 512, 2048])
+    X = residual_block(X, 3, [512, 512, 2048])
+
+    # Global Average Pooling to reduce spatial dimensions
+    X = GlobalAveragePooling2D()(X)
+    
+    # Add Dropout to prevent overfitting
+    X = Dropout(0.5)(X)
+    
+    # Fully Connected Layer for classification
+    X = Dense(classes, activation='linear', kernel_regularizer=l2(0.01))(X)
+        
+    # Create the model
+    model = Model(inputs = X_input, outputs = X, name='Modified_ResNet50')
+
+    return model
+
+# Define the shape of the input images and number of classes
+input_shape = (384, 384, 3)
+num_classes = 6
+
+# Initialize the modified ResNet50 model with the specified parameters
+Hinge_resnet50_model = Hinge_ResNet50(input_shape=input_shape, classes=num_classes)
+
+Hinge_resnet50_model.compile(optimizer='adam', loss='categorical_hinge', metrics=['accuracy'])
+
+
+# Add ReduceLROnPlateau callback
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=15, min_lr=0.00001)
+
+# Add EarlyStopping callback
+early_stopping = EarlyStopping(monitor='val_loss', mode='min', patience=50, restore_best_weights=True, verbose=1)
+
+
+# Total number of epochs
+num_epochs = 30
+
+# Train the model
+history = Hinge_resnet50_model.fit(train_generator, 
+                                      steps_per_epoch=len(train_generator), 
+                                      epochs=num_epochs, 
+                                      validation_data=val_generator, 
+                                      validation_steps=len(val_generator),
+                                      callbacks=[reduce_lr, early_stopping])
+
+
+Hinge_resnet50_model.save("resnet50_model_Hinge.h5")
+
+def evaluate_model_performance(model, val_generator, class_labels):
+    
+    # Getting all the true labels for the validation set
+    true_labels = val_generator.classes
+
+    # Get the class labels (names) from the generator
+    class_labels = list(val_generator.class_indices.keys())
+
+    # To get the predicted labels, we predict using the model  
+    predictions = model.predict(val_generator, steps=len(val_generator))
+    
+    # Take the argmax to get the predicted class indices.
+    predicted_labels = np.argmax(predictions, axis=1)
+    
+    # Extracting true labels from the validation generator
+    true_labels = val_generator.classes
+
+    # Classification report
+    report = classification_report(true_labels, predicted_labels, target_names=class_labels)
+    print(report)
+    print('\n')
+    
+    # Define a custom colormap
+    colors = ["white", "royalblue"]
+    cmap_cm = LinearSegmentedColormap.from_list("cmap_cm", colors)
+
+    # Confusion Matrix
+    cm = confusion_matrix(true_labels, predicted_labels)
+
+    # Plotting confusion matrix using seaborn
+    plt.figure(figsize=(8,6))
+    sns.heatmap(cm, annot=True, cmap=cmap_cm, fmt='d', xticklabels=class_labels, yticklabels=class_labels)
+    plt.xlabel('Predicted Labels')
+    plt.ylabel('True Labels')
+    plt.title('Confusion Matrix')
+    plt.show()
+    plt.savefig("scratch_confusion_matrix.png", bbox_inches='tight', dpi=300)
+
+evaluate_model_performance(Hinge_resnet50_model, val_generator, class_labels)
